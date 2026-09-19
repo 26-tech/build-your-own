e340284c98f0cca433a2e604ee6c0030d430f624
SUBHASREE SIL <26157033@kiit.ac.in>
2026-09-19 22:11:53 +0530

    0.0

---
+++ calculator.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, result;
+    char operator;
+
+    printf("=== Simple Calculator ===\n");
+    printf("Enter expression (e.g. 5 + 3): ");
+    scanf("%lf %c %lf", &num1, &operator, &num2);
+
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+
+        case '/':
+            if (num2 != 0.0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+
+        default:
+            printf("Error: Invalid operator. Use +, -, *, or /\n");
+    }
+
+    return 0;
+}
